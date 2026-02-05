1c86b6d74e51fc6388dd84714b55ae12d30fecf3
Chitra <johncdas1998@gmail.com>
2026-02-05 16:20:36 -0500

    added generator

--- orchestration/assets.py
+++ orchestration/assets.py
@@ -4,7 +4,9 @@ import os
 import sys
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from src.loading import snowflake_loader
+from src.data_generation import generator
 
 DBT_PROJECT_DIR = os.path.join(os.path.dirname(__file__), "..", "dbt")
 dbt_project = DbtProject(project_dir=DBT_PROJECT_DIR)
@@ -22,14 +24,13 @@ def raw_data_ingestion():
     Generates synthetic financial data and loads it into Snowflake.
     Produces the raw source tables for dbt.
     """
+    generator.main()
     snowflake_loader.main()
 
     yield MaterializeResult(asset_key=["openledger", "transactions"])
     yield MaterializeResult(asset_key=["openledger", "ledger_entries"])
     yield MaterializeResult(asset_key=["openledger", "settlements"])
     
-    return MaterializeResult(metadata={"info": "Ingestion Complete"})
-
 @dbt_assets(manifest=dbt_project.manifest_path)
 def dbt_analytics(context: AssetExecutionContext, dbt: DbtCliResource):
     yield from dbt.cli(["build"], context=context).stream()
