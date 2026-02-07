a6e437c8b2b9e7c4690be1d232654c84af31d222
Chitra <johncdas1998@gmail.com>
2026-02-07 09:41:03 -0500

    edited fact table

--- dbt/models/marts/fct_transactions.sql
+++ dbt/models/marts/fct_transactions.sql
@@ -1,14 +1,20 @@
 {{ config(
-    materialized='table',
-    unique_key='transaction_id'
+    materialized='incremental',
+    unique_key='transaction_id',
+    on_schema_change='fail'
 ) }}
 
 WITH internal AS (
-    SELECT * FROM {{ ref('staging_transactions') }}
+    SELECT * FROM {{ ref('stg_transactions') }}
 ),
 
 external AS (
-    SELECT * FROM {{ ref('staging_settlements') }}
+    SELECT * FROM {{ ref('stg_settlements') }}
+),
+
+integrity AS (
+    SELECT transaction_id, integrity_status
+    FROM {{ref('int_ledger_integrity')}}
 ),
 
 joined AS (
@@ -17,23 +23,28 @@ joined AS (
         COALESCE(i.transaction_id, e.transaction_id) AS transaction_id,
         
         -- Dimensions
-        i.user_id,
-        i.merchant_id,
-        i.event_time AS transaction_at,
+        COALESCE(i.user_id, 'UNKNOWN') AS user_id,
+        COALESCE(i.merchant_id, e.merchant_id) AS merchant_id,
+        COALESCE(i.event_time, e.settlement_date) AS transaction_at,
         e.settlement_date,
         
         -- Metrics
         i.amount AS internal_amount,
         e.net_amount AS bank_settled_amount,
         e.fee_amount AS bank_fee,
+
+        -- Integrity check
+        COALESCE(int.integrity_status, 'UNKNOWN') AS ledger_integrity,
         
         -- Calculating the difference (0 == perfect match)
         -- Handling null values by treating them as 0
-        ZEROIFNULL(e.net_amount) - ZEROIFNULL(i.amount) AS reconciliation_delta
+        ABS(ZEROIFNULL(e.net_amount) - ZEROIFNULL(i.amount) + ZEROIFNULL(e.fee_amount)) AS reconciliation_delta
         
     FROM internal i
     FULL OUTER JOIN external e 
         ON i.transaction_id = e.transaction_id
+    LEFT JOIN integrity int
+        ON i.transaction_id = int.transaction_id
 )
 
 SELECT
@@ -41,7 +52,11 @@ SELECT
     CASE
         WHEN internal_amount IS NOT NULL AND bank_settled_amount IS NULL THEN 'MISSING_EXTERNAL'
         WHEN internal_amount IS NULL AND bank_settled_amount IS NOT NULL THEN 'MISSING_INTERNAL'
-        WHEN internal_amount = bank_settled_amount THEN 'MATCHED'
+        WHEN reconciliation_delta < 0.02 THEN 'MATCHED'
         ELSE 'DISCREPANCY'
     END AS reconciliation_status
 FROM joined
+
+{% if is_incremental() %}
+    WHERE transaction_at >= (SELECT MAX(transaction_at) FROM {{this}})
+{% endif %}
